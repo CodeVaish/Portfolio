0ce8df04fb7c164479d814560d2b361db0061a32
CodeVaish <ayushishobhane@gmail.com>
2025-06-28 22:38:07 +0530

    Fix import casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "./index.css";
 
-import Nevbar from "./components/navbar/navbar";
+import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import About from "./components/about/About";
 import Projects from "./components/projects/Projects";
-import Mywork from "./components/mywork/mywork";
-import Contact from "./components/contact/contact";
-import Footer from "./components/footer/footer";
-import Education from "./components/myeducation/education";
+import Mywork from "./components/mywork/Mywork";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import Education from "./components/myeducation/Education";
 import Skills from "./components/skills/Skills";
 
 const App = () => {
   return (
     <div>
-      <Nevbar />
+      <Navbar />
       <Hero />
       <About />
       <Education />
